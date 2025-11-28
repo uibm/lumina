6d992f7f0e96e87a63a9fef7fac8f5e4139b38cc
Ujjwal Kumar <ujjwal.kumar1@ibm.com>
2025-11-28 15:13:12 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://uibm.github.io',
+  site: 'https://uibm.github.io/lumina',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
