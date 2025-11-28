d020fcf6a629b0250dc33cffdbb94d43a5d2a717
Ujjwal Kumar <ujjwal.kumar1@ibm.com>
2025-11-28 15:17:58 +0530

    removed node modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://uibm.github.io/lumina',
+  site: 'https://uibm.github.io',
+  base: '/lumina',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
